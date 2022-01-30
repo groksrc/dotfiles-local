304d668a56764658fb6b4f2cb2ef0c6688871e54
Drew Cain <groksrc@gmail.com>
2022-01-30 10:31:58 -0600

    mkdir code

--- boot.sh
+++ boot.sh
@@ -1,8 +1,10 @@
-# install 1Password
-brew install --cask 1password
+# make code
+mkdir -p code;
 
-read -p "1Password is installed. Ready to continue?" -n 1 -r
-echo    # (optional) move to a new line
+# install 1Password
+brew install --cask 1password;
+
+read -p "1Password is installed. Ready to continue?" -n 1 -r;
 if [[ ! $REPLY =~ ^[Yy]$ ]]
 then
     [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
